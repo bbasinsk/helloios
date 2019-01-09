ba1bf1a4c0894adf05d2b0f6e3bc34bb54fed33d
Ben Basinski <benbasinski97@gmail.com>
2019-01-08 19:31:11 -0800

    Initial Commit

---
+++ helloios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  helloios
+//
+//  Created by Ben Basinski on 1/8/19.
+//  Copyright © 2019 Ben Basinski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
